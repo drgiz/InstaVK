580b5814da97fe53dfec0be1c89adad7f260d2d6
Никита <nikita@h235.net53.bmstu.ru>
2017-03-22 01:18:53 +0300

    Начало

---
+++ InstaVK/InstaVK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaVK
+//
+//  Created by Никита on 22.03.17.
+//  Copyright © 2017 Nikita Susoev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
