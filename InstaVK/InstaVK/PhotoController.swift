4343eeb56586235909a93595811c835e7990c267
Никита <nikita@h10.net55.bmstu.ru>
2017-03-27 23:51:39 +0300

    Prototype interface

---
+++ InstaVK/InstaVK/PhotoController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoController.swift
+//  InstaVK
+//
+//  Created by Никита on 27.03.17.
+//  Copyright © 2017 Nikita Susoev. All rights reserved.
+//
+
+import UIKit
+
+class PhotoController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
