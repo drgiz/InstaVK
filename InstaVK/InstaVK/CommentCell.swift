12b7617800a55f2c759e3aab580823463482f9fc
Svyatoslav Bykov <gizzerman@gmail.com>
2017-04-04 12:58:49 +0300

    added xib for CommentCell

---
+++ InstaVK/InstaVK/CommentCell.swift
@@ -0,0 +1,25 @@
+//
+//  CommentCell.swift
+//  InstaVK
+//
+//  Created by Svyatoslav Bykov on 02.04.17.
+//  Copyright © 2017 Svyatoslav Bykov. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var commentText: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
