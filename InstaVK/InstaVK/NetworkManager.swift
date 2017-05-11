80ff3f427a52bfd9bb6a8b09e17e57fe50fa3c35
Svyatoslav Bykov <gizzerman@gmail.com>
2017-05-11 04:59:57 +0300

    moved network files into Network Manager, tbd

--- InstaVK/InstaVK/NetworkManager.swift
+++ InstaVK/InstaVK/NetworkManager.swift
@@ -3,13 +3,41 @@
 //  InstaVK
 //
 //  Created by Svyatoslav Bykov on 08.05.17.
-//  Copyright © 2017 Nikita Susoev. All rights reserved.
+//  Copyright © 2017 InstaVK. All rights reserved.
 //
-
+import UIKit
 import VK_ios_sdk
 
-class NetworkManager {
-    
-    //TO-DO: Move url/sdk network logic here
-    
+func vkApiUrlBuilder(vkApiMethod: String, queryItems: [String:String]...) -> URL? {
+    let components = NSURLComponents()
+    components.scheme = "https"
+    components.host = "api.vk.com"
+    components.path = "/method/"+vkApiMethod
+    var items = [URLQueryItem]()
+    for item in queryItems {
+        for (name, value) in item {
+            items.append(URLQueryItem(name: name, value: value))
+        }
+    }
+    components.queryItems = items
+    if let url = components.url {
+        return url
+    } else {
+        return nil
+    }
 }
+
+//TEST
+func getUsers() {
+    let request: VKRequest = VKApi.friends().get(["order":"name", "count":3, "fields":"domain, photo_100" ])
+    request.execute(resultBlock: { (response) -> Void in
+        guard let dictionaries = response?.json as? [String:Any] else { return }
+        dictionaries.forEach({ (key, value) in
+            guard let dictionary = value as? [String: Any] else { return }
+            print(dictionary)
+        })
+    },errorBlock: {(_ error: Error?) -> Void in
+        print("Error: \(error.debugDescription)")
+    })
+}
+
